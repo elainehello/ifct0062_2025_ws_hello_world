6fe3a60305a32fc868c775b02776cc6ce08779b3
elainehello <195331169+elainehello@users.noreply.github.com>
2025-05-16 19:48:10 +0200

    dockerfile included

--- src/main/java/com/elainevalles/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
+++ src/main/java/com/elainevalles/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
@@ -1,5 +1,8 @@
 package com.elainevalles.ifct0062_2025_ws_hello_world.controller;
 
+import com.elainevalles.ifct0062_2025_ws_hello_world.model.Saludo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -9,4 +12,11 @@ public class SaludadorController {
 
     public static final String template = "Hola, %s!";
     private final AtomicLong counter = new AtomicLong(); //avoid memory or call inconsistency in data
+
+    @GetMapping("/saludo")
+    public Saludo saludo(@RequestParam(value= "nombre", defaultValue="World") String nombre) {
+        return new Saludo(counter.incrementAndGet(), String.format(template, nombre));
+    }
+
+
 }
